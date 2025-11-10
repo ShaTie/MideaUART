a9b900b15d8e36a48ae9216db5c075edfb15f93d
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2025-11-10 08:41:49 +0400

    Inline IDGen

--- include/Helpers/IDGenerator.hpp
+++ include/Helpers/IDGenerator.hpp
@@ -7,7 +7,7 @@ namespace helpers {
 class IDGenerator {
  public:
   uint8_t get() const { return m_id; }
-  uint8_t next();
+  uint8_t next() { return ++m_id ? m_id : ++m_id; }
 
  private:
   uint8_t m_id{};
--- src/Helpers/IDGenerator.cpp
+++
@@ -1,7 +0,0 @@
-#include "Helpers/IdGenerator.hpp"
-
-namespace helpers {
-
-uint8_t IDGenerator::next() { return ++m_id ? m_id : ++m_id; }
-
-}  // namespace helpers
